eea3a5f13f0b023e981eee58d5e1f2a40fee4ccc
Sébastien Marmin <33869096+SebastienMarmin@users.noreply.github.com>
2019-07-29 12:57:36 +0000

    cheaper test

--- tests/run_all.py
+++ tests/run_all.py
@@ -9,7 +9,7 @@ exe_files.remove(os.path.basename(os.path.realpath(__file__)))
 
 for exe_file in exe_files:
     if exe_file == "test_gp.py":
-        command = "--dataset simple2 --dataset_dir /home/sebastien/Datasets/ --nmc_train 100 --nmc_test 200 --batch_size 100 --lr 5e-3 --iterations_fixed_noise 1000 --iterations_free_noise 0 --model fgp --test_interval 1000 --nfeatures 50 --verbose --noise_std 2e-2 --full_cov_W 1".split()
+        command = "--dataset simple2 --dataset_dir ./test_datasets/ --nmc_train 20 --nmc_test 25 --batch_size 100 --lr 5e-2 --iterations_fixed_noise 50 --iterations_free_noise 0 --model fgp --test_interval 50 --nfeatures 15 --verbose --noise_std 2e-2 --full_cov_W 1".split()
     else:
         command = [""]
     
